3db4f0007598a279cb617cf5fe5f88db8193ed88
Divyanshu Mani Tripathi <divyanshumanitripathi@gmail.com>
2020-02-06 12:07:43 +0530

    Update wolfram_process.php

--- php/wolfram_process.php
+++ php/wolfram_process.php
@@ -1,8 +1,1 @@
-<?php
-//paster APIkey/ APPid in line number 5.
-$question=$_GET['question'];
-$question = preg_replace('/\s+/', '%20', $question);
-$wolfram_api_url ="http://api.wolframalpha.com/v1/spoken?appid=9HWT3Y-LQ6A52TTRA&i=".$question."&units=metric";
-$result = file_get_contents($wolfram_api_url);
-echo $result;
-?>
+
